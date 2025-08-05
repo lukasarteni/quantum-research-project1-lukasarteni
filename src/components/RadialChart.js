88b2529fb9720b89fc97b22893fbec7d77b018a3
lukas-arteni-rutgers <lva9@rutgers.edu>
2025-08-05 22:05:40 +0000

    maybve works?

--- src/components/RadialChart.js
+++ src/components/RadialChart.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+//import React, { useEffect, useRef } from "react";
 
 const RadialChart = ({ data, width = 300, height = 300 }) => {
   const ref = useRef();
