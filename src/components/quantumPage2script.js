1ea03d76e46397c662fe90f7a705815f6513df41
lukas-arteni-rutgers <lva9@rutgers.edu>
2025-08-31 10:30:11 +0000

    too late

--- src/components/quantumPage2script.js
+++ src/components/quantumPage2script.js
@@ -1,3 +1,4 @@
+import { Input } from 'free-astro-components';
 import Plot from 'react-plotly.js';
 
 var data = [
@@ -74,9 +75,84 @@ document
     alert("hi");
   });
 
+
+var alphaAngle = 0;
+var target1 = document
+  .getElementById("alphaAnglePicker");
+target1
+  .addEventListener("change", () => {
+    //alert("hi" + target1.value);
+    alphaAngle = Number(target1.value);
+    changeData(alphaAngle, 0);
+    update();
+  });
+var betaAngle = 22.5;
+var angleDiffVar = 22.5;
+var target2 = document
+  .getElementById("betaAnglePicker");
+target2
+  .addEventListener("change", () => {
+    betaAngle = Number(target2.value);
+    changeData(betaAngle, 1);
+    update();
+  });
+
+
+function update() {
+  angleDiffVar = angleDiffernce(alphaAngle, betaAngle);
+  document.getElementById("diffInAngleText").textContent = angleDiffVar ;
+  winPercentMethond(alphaAngle, betaAngle);
+}
 function changeData(angle, line) {
+  //alert("theta = " +angle+ " and " + (angle +90));
+
   Plotly.restyle('qpage2chart1', { 'theta': [[0, angle, 0, angle + 90, 0]] }, [line])
 }
+function angleDiffernce(angle1, angle2) {
+  return (Math.abs((angle1 - angle2 + 180) % 360 - 180));
+}
+function probABareEqual(num1, num2) {
+  return Math.pow(Math.cos(Math.PI / 180 * (num1 - num2)), 2);
+}
+function probABareDiff(num1, num2) {
+  return Math.pow(Math.sin(Math.PI / 180 * (num1 - num2)), 2);
+}
+function probABareEqual2(nums) {
+  return Math.pow(Math.cos(Math.PI / 180 * (nums)), 2);
+}
+function probABareDiff2(nums) {
+  return Math.pow(Math.sin(Math.PI / 180 * (nums)), 2);
+}
+/*
+function winPercentMethond(a0, b0) {
+  //alert(Math.cos(Math.PI));
+  const a1 = Number(a0 + 90);
+  const b1 = Number(b0 + 90);
+  const E00 = probABareEqual(a0, b0);
+  const E01 = probABareEqual(a0, b1);
+  const E10 = probABareEqual(a1, b0);
+  const E11 = probABareDiff(a1, b1);
+  //alert(E00 + " " + E01 + " " + E10 + " " + E11);
+  const Svalue = E00 + E01 + E10 + E11;
+  //alert(Svalue);
+  //const result = 0.5 + Svalue / 8;
+  //const result = (3* E00 +E11)/4 *100;
+  const result= E00+E01+E10+E11;
+  document.getElementById("winPercenttext").textContent = result;
+
+}*/
+function winPercentMethond(a0, b0) {
+  
+  const a1 = Number(a0 + 90);
+  const b1 = Number(b0 + 90);
+  const angle1 = angleDiffVar
+  const angle2 = 90 - angle1;
+  //NOT REAL
+  const result = (3 * probABareEqual2(angle1) + probABareDiff2(angle2))/4;
+  document.getElementById("winPercenttext").textContent = result;
+  //not working
+}
+
 
 Plotly.newPlot('qpage2chart1', data, layout);
 
